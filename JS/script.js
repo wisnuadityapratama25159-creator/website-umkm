defe282c97cfeb2344da99428e088f5521a27549
Wisnu Aditya Pratama <wisnu.aditya.pratama25159@mhs.uingusdur.ac.id>
2026-09-13 22:32:34 +0700

    feat: tambah informasi lokasi umkm

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+const promoButton = document.querySelector("#promoButton");
+
+promoButton.addEventListener("click", () => {
+  promoButton.textContent = "Promo Warkop Mas Hamzah";
+  console.log("Promo Warkop Mas Hamzah berhasil ditampilkan, Ngopi Kuy!.");
+});
--- JS/script.s
+++
@@ -1,6 +0,0 @@
-const promoButton = document.querySelector("#promoButton");
-
-promoButton.addEventListener("click", () => {
-  promoButton.textContent = "Promo: Beli 2 gratis tester!";
-  console.log("Promo Kopi Nusa berhasil ditampilkan.");
-});
